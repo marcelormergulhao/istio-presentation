874bbe84cb6e1bf9e7b6a0fe8b60920e728f1e3a
Marcelo Mergulhão <marcelormergulhao@gmail.com>
2019-12-08 23:07:03 -0300

    Add script to generate productpage traffic

---
+++ scripts/call_productpage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
+export INGRESS_HOST=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath='{.items[0].status.hostIP}')
+export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+watch -n 1 curl -o /dev/null -s -w %{http_code} $GATEWAY_URL/productpage
